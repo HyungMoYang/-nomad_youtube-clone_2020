272ea397afed7abdd43cff1442e553ace09e41f6
TipsyBoy <gudah13@naver.com>
2020-11-23 19:41:30 +0900

    track git nico

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,13 @@
-import express from "express"
-
+import express from "express";
+import { editProfile } from "../controller/userController";
+import { deleteVideo, upload, videoDetail, videos } from "../controller/videoController";
+import routes from "../routes";
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editProfile, editProfile);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
