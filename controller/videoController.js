3b05eef161cc1074c348320d38636d13e4135706
TipsyBoy <gudah13@naver.com>
2020-12-02 16:03:44 +0900

    Uploading and Creating a Video part Two

--- controller/videoController.js
+++ controller/videoController.js
@@ -29,17 +29,36 @@ export const getUpload = (req, res) => {
     res.render("upload", { pageTitle: "Upload" });
 }
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
     const {
-        body: { file, title, description }
+        body: { title, description },
+        file: { path }
     } = req;
 
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    })
+    console.log(newVideo)
+    res.redirect(routes.videoDetail(newVideo.id))
     // To-do: Upload video and save to DB
-
-    res.redirect(routes.videoDetail(2134123))
 }
 
-export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async (req, res) => {
+    const {
+        params: { id }
+    } = req;
+    try {
+        const video = await Video.findById(id);
+        res.render("videoDetail", { pageTitle: "Video Detail", video });
+    } catch (error) {
+        console.log(error)
+        res.redirect(routes.home)
+    }
+
+
+}
 
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
 
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { editProfile } from "../controller/userController";
 import { deleteVideo, editVideo, getUpload, postUpload, upload, videoDetail } from "../controller/videoController";
+import { uploadVideo } from "../middlewares";
 import routes from "../routes";
 const videoRouter = express.Router();
 
 // upload videos
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editProfile, editProfile);
